24f32c8dced013544576b31577ed868ad12ef468
j-labbe <86276115+j-labbe@users.noreply.github.com>
2023-07-11 09:56:18 -0400

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
 const pkg = require("./package.json");
 
 const createCipheriv = crypto.createCipheriv;
 const createDecipheriv = crypto.createDecipheriv;
 const randomBytes = crypto.randomBytes;
 
-let jwtSecret = "Secret (this will be overridden by user at launch)";
-let passSecret = "Secret (this will be overridden by user at launch)";
+let passSecret = "YR4MRnCWhBEqwMCgEu7mDcWzu4FMUQkv";
 
 const encrypt = async (password) => {
+    if (!passSecret) {
+        throw new Error("Invalid password secret key. A secret key is required to encrypt passwords in the database. Please define a secret key in your .env file using PASSWORD_SECRET_KEY");
+    }
     const iv = Buffer.from(randomBytes(16));
     const cipher = createCipheriv("aes-256-gcm", Buffer.from(passSecret), iv);
     const encpass = Buffer.concat([cipher.update(password), cipher.final()]);
@@ -21,6 +22,9 @@ const encrypt = async (password) => {
 };
 
 const decrypt = async (encpass) => {
+    if (!passSecret) {
+        throw new Error("Invalid password secret key. A secret key is required to encrypt passwords in the database. Please define a secret key in your .env file using PASSWORD_SECRET_KEY");
+    }
     const decipher = createDecipheriv(
         "aes-256-gcm",
         Buffer.from(passSecret),
@@ -33,25 +37,32 @@ const decrypt = async (encpass) => {
 
 const EncryptionTools = {
     /**
-     * @param {string} password Password to be encrypted
-     * @returns Signed JsonWebToken
+     * @param password Password to be encrypted
+     * @returns JSON object containing encrypted password & metadata
      */
     encryptPassword: async (password) => {
         const encryptedPass = await encrypt(password);
-        const token = jwt.sign(encryptedPass, jwtSecret);
-        return token;
+        const jsonString = JSON.stringify(JSON.stringify(encryptedPass));
+        return jsonString;
     },
     /**
      * 
-     * @param {string} token JsonWebToken
+     * @param jsonString Stringified JSON containing encrypted password & metadata
      * @returns Decrypted password
      */
-    decryptPassword: async (token) => {
-        const isTrusted = jwt.verify(token, jwtSecret);
-        if (isTrusted) {
-            const decoded = jwt.decode(token);
-            return await decrypt(decoded);
-        }
+    decryptPassword: async (jsonString) => {
+        const parsedJson = JSON.parse(JSON.parse(jsonString));
+        return await decrypt(parsedJson);
+    },
+    /**
+     * 
+     * @param passwordAttempt The password that is being checked as a match
+     * @param jsonString Stringified JSON object containing encrypted password & metadata
+     */
+    verifyMatch: async (passwordAttempt, jsonString) => {
+        const parsedJson = JSON.parse(jsonString);
+        const password = await decrypt(parsedJson);
+        return password === passwordAttempt;
     }
 }
 
@@ -61,35 +72,29 @@ console.log(`${pkg.name} - v${pkg.version} - Author: ${pkg.author}\n`);
 
 readLine.question("Mode (enc / dec)\n> ", (mode) => {
     if (mode === "enc") {
-        readLine.question("Enter a JWT secret.\n> ", (secret) => {
-            jwtSecret = secret;
-            readLine.question("Enter a password secret (32 bytes - 32 chars).\n> ", (secret) => {
-                if (secret.length !== 32) {
-                    console.log("Password secret must be 32 characters (32 bytes)!");
-                    return readLine.close();
-                }
-                passSecret = secret;
-                readLine.question("Enter a password you would like to be encrypted.\n> ", async (data) => {
-                    const jwt = await EncryptionTools.encryptPassword(data);
-                    console.log(`\nYour JSONWebToken is: ${jwt}`);
-                    readLine.close();
-                });
+        readLine.question("Enter a password secret (32 bytes - 32 chars).\n> ", (secret) => {
+            if (secret.length !== 32) {
+                console.log("Password secret must be 32 characters (32 bytes)!");
+                return readLine.close();
+            }
+            passSecret = secret;
+            readLine.question("Enter a password you would like to be encrypted.\n> ", async (data) => {
+                const jwt = await EncryptionTools.encryptPassword(data);
+                console.log(`\nYour output is: ${jwt}`);
+                readLine.close();
             });
         });
     } else if (mode === "dec") {
-        readLine.question("Enter a JWT secret.\n> ", (secret) => {
-            jwtSecret = secret;
-            readLine.question("Enter a password secret.\n> ", (secret) => {
-                if (secret.length !== 32) {
-                    console.log("Password secret must be 32 characters (32 bytes)!");
-                    return readLine.close();
-                }
-                passSecret = secret;
-                readLine.question("Enter the JWT that contains encrypted password object.\n> ", async (data) => {
-                    const jwt = await EncryptionTools.decryptPassword(data);
-                    console.log(`\nYour decrypted password is: ${jwt}`);
-                    readLine.close();
-                });
+        readLine.question("Enter a password secret.\n> ", (secret) => {
+            if (secret.length !== 32) {
+                console.log("Password secret must be 32 characters (32 bytes)!");
+                return readLine.close();
+            }
+            passSecret = secret;
+            readLine.question("Enter the JWT that contains encrypted password object.\n> ", async (data) => {
+                const jwt = await EncryptionTools.decryptPassword(data);
+                console.log(`\nYour decrypted password is: ${jwt}`);
+                readLine.close();
             });
         });
     } else {
